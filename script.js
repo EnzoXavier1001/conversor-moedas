1f185f84737c6abb13d184092de3f79449019713
Enzo Costa <enzo_costa7@hotmail.com>
2022-10-16 12:40:48 -0300

    refactor the functions for currency conversion

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-async function converter() {
-    const amount = document.querySelector('[data-amount]').value
-    const optionSelected = document.querySelector('[data-option]').value
-    const result = document.querySelector('[data-result]')
-    let cotacao;
-    let res;
+const showResult = document.querySelector('[data-result]')
+const optionSelected = document.querySelector('[data-option]').value
 
-    switch(optionSelected) {
+let cotacao;
+let res;
+
+function converter() {
+    const amount = document.querySelector('[data-amount]').value
+
+    if(amount != '') {
+        checkChosenOption(optionSelected, amount)
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Insira um valor para converter'
+          })
+    }
+}
+
+async function checkChosenOption(opcao, valor) {
+    switch(opcao) {
         case 'dolar':
             cotacao = await fetchApi('https://economia.awesomeapi.com.br/json/last/USD-BRL')
-            res = cotacao.USDBRL.high * amount
-            result.textContent = ' ' + res.toFixed(2)
+            const { high } = cotacao.USDBRL
+            res = high * valor
+            showResult.textContent = ' ' + res.toFixed(2)
         break;
         case 'euro':
             cotacao = await fetchApi('https://economia.awesomeapi.com.br/json/last/EUR-BRL')
-            res = cotacao.EURBRL.high * amount
-            result.textContent = ' ' + res.toFixed(2)
-            break;
+            high = cotacao.EURBRL
+            res = high * valor
+            showResult.textContent = ' ' + res.toFixed(2)
+        break;
     }
 }
 
